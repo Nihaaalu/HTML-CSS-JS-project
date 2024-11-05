accd8135eb64ab1ce834d1bc610b848fd7404cea
Mahammad Nihal T U <156331518+Nihaaalu@users.noreply.github.com>
2024-11-05 09:36:14 +0530

    update

--- edit ninja/html/js.js
+++ edit ninja/html/js.js
@@ -6,7 +6,7 @@ createLinkButton.addEventListener('click', function() {
   const inf2 = encodeURIComponent(document.getElementById('inf2').value);
   const inf3 = encodeURIComponent(document.getElementById('inf3').value);
 
-  const whatsAppLink = `https://wa.me/9110242527?text=${message1}%20${message2}%20${message3}`;
+  const whatsAppLink = `https://wa.me/9110242527?text=${inf1}%20${inf2}%20${inf3}`;
 
   whatsappLink.href = whatsAppLink;
   whatsappLink.textContent = whatsAppLink;
